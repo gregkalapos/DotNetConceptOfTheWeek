dbb5152cc507a071337cba59d9d35c9c969d1d95
Gergely Kalapos <gergo@kalapos.net>
2018-01-21 18:06:16 +0100

    Adding sample code for episode 3 (C# 8 nullable reference types)

---
+++ Cs8NullableReferenceTypes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs8NullableReferenceTypes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person? p = null;
+			if (p == null)
+				return;
+
+			Console.WriteLine(p);
+		}
+
+		public static void PrintMiddleName(Person p)
+		{
+			Console.WriteLine(p.MiddleName!);
+		}
+	}
+	class Person
+	{
+		public Person(String firstName, String lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+		public String FirstName { get; set; }
+		public String? MiddleName { get; set; }
+		public String LastName { get; set; }
+	}
+}
