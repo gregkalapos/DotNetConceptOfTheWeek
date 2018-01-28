f26d7bf69b86083fe50dadbda09183dcf423bb6d
Gergely Kalapos <gergo@kalapos.net>
2018-01-28 22:11:59 +0100

    Cleaning up covariance/contravariance sample

--- CovarianceAndContravarianceSample/Program.cs
+++ CovarianceAndContravarianceSample/Program.cs
@@ -4,15 +4,9 @@ using System.Collections.Generic;
 
 namespace CovarianceAndContravarianceSample
 {
-	public class Animal : IAbleToFeed<Animal>
+	public class Animal 
 	{
 		public void Eat() => Console.WriteLine("Eat");
-
-		public void Feed(Animal t)
-		{
-			Console.WriteLine("Feeding");
-			t.Eat();
-		}
 	}
 
 	public class Bird : Animal
@@ -20,122 +14,79 @@ namespace CovarianceAndContravarianceSample
 		public void Fly() => Console.WriteLine("Fly");
 	}
 
+    public class Human: Animal{}
+
 	delegate Animal ReturnAnimalDelegate();
 	delegate Bird ReturnBirdDelegate();
 
+    delegate void TakeAnimalDelegate(Animal a);
+    delegate void TakeBirdDelegate(Bird b);
 
-	delegate void TakeAnimalDelegate(Animal a);
-	delegate void TakeBirdDelegate(Bird b);
+    interface IProcess<out T>
+    {
+        T Process();
+    }
 
-	interface IAbleToFeed<in T>
-	{
-		void Feed(T t);
-	}
+    public class AnimalProcessor<T> : IProcess<T>
+    {
+        public T Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
 
-	interface IZoo<in T>
-	{
-		void Add(T t);
-	}
+    interface IZoo<in T>
+    {
+        void Add(T t);
+    }
 
-	public class Zoo<T> : IZoo<T>
-	{
-		public void Add(T t)
-		{
-			
-		}
-	}
+    public class Zoo<T> : IZoo<T>
+    {
+        public void Add(T t)
+        {
 
-	interface IProcessItem<out T>
-	{
-		T Process();
-	}
+        }
+    }
 
-	public class AnimalProcessor<T> : IProcessItem<T>
-	{
-		public T Process()
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	class Human : Animal
-	{
-
-	}
-	class Program
+    class Program
 	{
 		public static Bird GetBird() => new Bird();
 		public static Animal GetAnimal() => new Animal();
 
-
-		public static void Eat(Animal a) => a.Eat();
-
-		public static void Fly(Bird b) => b.Fly();
+        public static void Eat(Animal a) => a.Eat();
+        public static void Fly(Bird b) => b.Fly();
 
 		static void Main(string[] args)
 		{
-			//IComparer - contravarinace
-			IZoo<Animal> animalZoo = new Zoo<Animal>(); //GetANimalZoo
-			IZoo<Bird> birdZoo = new Zoo<Bird>(); //GetBirdZoo
-			birdZoo = animalZoo;
-			
-			IAbleToFeed<Human> a = new Bird();
-			a.Feed(new Human());
-			
-			//IEnumerator - covariant
-			IProcessItem<Animal> animalProcessor = new AnimalProcessor<Animal>();
-			IProcessItem<Bird> birdProcessor = new AnimalProcessor<Bird>();
+            //delegates covariance
+            Animal a = new Bird();
+            ReturnAnimalDelegate d = GetBird;
 
-			IEnumerable<Animal> alist = new List<Bird>();
-			animalProcessor = birdProcessor;
+            //Delegates contravariance
+            TakeBirdDelegate d2 = Eat;
+            d2(new Bird());
 
-			//Animal[] animalArray = new Bird[10];
-			//animalArray[0] = new Human();
+            //arrays covariance
+            Animal[] animals = new Bird[10];
+            //This is broken:
+            //animals[0] = new Human(); 
 
+            //generics covariance
+            IProcess<Animal> animalProcessor = new AnimalProcessor<Animal>();
+            IProcess<Bird> birdProcessor = new AnimalProcessor<Bird>();
 
-			//IEnumerable<Animal> cc = new List<Bird>();
+            Animal animal = birdProcessor.Process();
+            animalProcessor = birdProcessor;
 
+            IEnumerable<Animal> animalList = new List<Bird>();
 
-			//IGetBest<Animal> bb = new GetBestAnimal();
-
-			//var pp = bb.MyMethod();
-
-
-			//Animal[] aa = new Bird[5];
-			//aa[1] = new Giraffe();
-
-
-
-			//ProcessBirdDelegate dd = EatAnimal;
-			//dd(new Bird());
-
-			//IEnumerable<int>
-			//Func<int,int>
-
-
-
-			//dd(new Fly)
-
-			//ProcessBirdDelegate d1 = EatAnimal;
-			//ProcessBirdDelegate d2 = FlyBird;
-
-
-
-			//d1(new Bird());
+            //generic contravariance
+            IZoo<Animal> animalZoo = new Zoo<Animal>();
+            animalZoo.Add(new Animal());
+            IZoo<Bird> birdZoo = new Zoo<Bird>();
+           
+            birdZoo = animalZoo;
+            birdZoo.Add(new Bird());
 		}
 	}
-
-	//interface IGetBest<out T>
-	//{
-
-	//	T GetBestItem();
-	//}
-
-	//class GetBestAnimal : IGetBest<Animal>
-	//{
-	//	public Animal GetBestItem()
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-	//}
 }
