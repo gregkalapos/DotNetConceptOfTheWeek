fd8c423a3e7a95b4d1d49b05b47dc97a9a789ee6
Gergely Kalapos <gergo@kalapos.net>
2018-02-10 18:51:32 +0100

    Added sample code for episode 6 - ASP.NET Core social login

---
+++ AspNetCore_SocialLogin/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_SocialLogin.Controllers
+{
+    public class AuthController : Controller
+    {
+		private readonly IAuthenticationSchemeProvider authenticationSchemeProvider;
+
+		public AuthController(IAuthenticationSchemeProvider authenticationSchemeProvider)
+		{
+			this.authenticationSchemeProvider = authenticationSchemeProvider;
+		}
+
+        public async Task<IActionResult> Login()
+        {
+			var allSchemeProvider = (await authenticationSchemeProvider.GetAllSchemesAsync())
+				.Select(n => n.DisplayName).Where(n => !String.IsNullOrEmpty(n));
+
+			return View(allSchemeProvider);
+        }
+
+		public IActionResult SignIn(String provider)
+		{
+			return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+		}
+
+		public async Task<IActionResult> SignOut()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
---
+++ AspNetCore_SocialLogin/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore_SocialLogin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			}).AddTwitter(twitterOptions =>
+			{
+				twitterOptions.ConsumerKey = ""; //TODO: Fill with your data from twitter
+				twitterOptions.ConsumerSecret = ""; //TODO: Fill with your data from twitter
+			})
+			.AddFacebook(facebookOptions =>
+			{
+				facebookOptions.AppId = ""; //TODO: Fill with your data from facebook
+				facebookOptions.AppSecret = ""; //TODO: Fill with your data from facebook
+			})
+			.AddCookie();
+			
+			services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+			app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
