84fe438e01920072666e6e39d3eb936ead3b21e0
Gergely Kalapos <gergo@kalapos.net>
2018-04-23 21:55:47 +0200

    Adding ANCM sample

---
+++ 12_ANCM_AspNetCore21/ANCM_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANCM_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
