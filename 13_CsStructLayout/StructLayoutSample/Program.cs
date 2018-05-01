5356a8cbf5d648754d32b9c7bfb34ba40cb97bc6
Gergely Kalapos <gergo@kalapos.net>
2018-05-01 22:52:01 +0200

    Adding C# struct alignment sample code (Ep. 13)

---
+++ 13_CsStructLayout/StructLayoutSample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace StructLayoutSample
+{
+	public class Program
+	{
+		struct Struct1
+		{
+			public byte b1;
+			public double d1;
+			public byte b2;
+			public double d2;
+			public byte b3;
+		}
+		
+		[StructLayout(LayoutKind.Auto)]
+		struct Struct2
+		{
+			public double d1;
+			public double d2;
+			public byte b1;
+			public byte b2;
+			public byte b3;
+		}
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		struct Struct3
+		{
+			public byte b1;
+			public double d1;
+			public double d2;
+			public byte b2;
+			public byte b3;
+		}
+
+
+		[StructLayout(LayoutKind.Explicit)]
+		struct Union
+		{
+			[FieldOffset(0)]
+			public byte b;
+			[FieldOffset(0)]
+			public int i;
+			[FieldOffset(0)]
+			public bool boolean;
+		}
+
+		class C { }
+
+		static void Main(string[] args)
+		{
+			unsafe
+			{
+				Console.WriteLine("Struct1: " + sizeof(Struct1));
+				Console.WriteLine("Struct2: " + sizeof(Struct2));
+				Console.WriteLine("Struct3: " + sizeof(Struct3));
+
+				Console.WriteLine("Union: " + sizeof(Union));
+			}
+			Console.ReadKey();
+		}
+	}
+}
